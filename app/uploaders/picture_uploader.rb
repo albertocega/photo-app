84c6e1ab6360fa9c0df84e8b886c7cc7df433b3e
Benno Celorio <benno.celorio@gmail.com>
2018-01-06 23:08:35 +0000

    prueba file

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,8 +7,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
   
-    storage :fog
-    #storage :file
+    #storage :fog
+    storage :file
   else
   
     storage :file
